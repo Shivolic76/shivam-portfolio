3350193b6aad984be248b53dc0056cb59eaf9863
Shivam Chudasama <49450011+Shivolic76@users.noreply.github.com>
2023-05-29 11:35:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# shivam-portfolio
+
+### A clean portfolio template. (Readme will be updated soon)
+
+# Sections
+
+- Home
+- Education and Certificates
+- Experience
+- Projects
+- Contact and Resume
+
+# How To Use
+
+- Clone this repository (or fork, then clone your fork :) )
+- Run `npm i`
+- Check it out using `npm start`
+
+# How Do I Customize
+
+- Replace `homepage` in package.json to your domain name or `https://<username>.github.io`
+- In `src/portfolio.js` you can add your personal portfolio details.
+- In `src/theme.js` you can change the theme colors. You can change between Light and Dark theme with the theme switch on the header.
+
+# References
+
+Based on https://github.com/ashutosh1919/masterPortfolio/ and https://github.com/saadpasta/developerFolio
+Illustrations: https://undraw.co/
---
+++ src/portfolio.js
@@ -0,0 +1,360 @@
+/* Change this file to get your personal Porfolio */
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Shivam",
+  logo_name: "Shivam.c()",
+  nickname: "Shiv",
+  full_name: "Shivam Chudasama",
+  subTitle: "ReactJs Developer. Always learning,",
+  resumeLink:
+    "https://drive.google.com/file/d/1IbKWAgUc0BC72a4ucujUUbwmcuTUZk5z/view?usp=sharing",
+  mail: "mailto:chudasamashivamkumar@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  linkedin: "https://www.linkedin.com/in/shiv-chudasama-415a50133/",
+  gmail: "chudasamashivamkumar@gmail.com",
+  github: "https://github.com/Shivolic76",
+  gitlab: " ",
+  facebook: " ",
+  twitter: " ",
+};
+
+const skills = {
+  data: [
+    {
+      title: "ReactJs Developer",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Creating interactive and user-friendly web interfaces using ReactJS ",
+        "⚡ Building single-page applications (SPAs) with ReactJS for a smooth and seamless user experience ",
+        "⚡ Implementing responsive web design techniques to ensure compatibility across various devices and screen sizes ",
+        "⚡ Integrating RESTful APIs with ReactJS to fetch and display data in real-time ",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "BootStrap",
+          fontAwesomeClassname: "simple-icons:bootstrap",
+          style: {
+            color: "#7952B3",
+          },
+        },
+        {
+          skillName: "Tailwind CSS",
+          fontAwesomeClassname: "simple-icons:tailwindcss",
+          style: {
+            color: "#38B2AC",
+          },
+        },
+        {
+          skillName: "Material UI",
+          fontAwesomeClassname: "simple-icons:mui",
+          style: {
+            color: "#1976d2",
+          },
+        },
+        {
+          skillName: "GatsbyJs",
+          fontAwesomeClassname: "simple-icons:gatsby",
+          style: {
+            color: "#663399",
+          },
+        },
+        {
+          skillName: "Ant Design",
+          fontAwesomeClassname: "simple-icons:antdesign",
+          style: {
+            color: "#1890ff",
+          },
+        },
+        {
+          skillName: "SASS",
+          fontAwesomeClassname: "simple-icons:sass",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#21759B",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "L. D. College of Engineering endearing, Ahmedabad",
+      subtitle: "B.Tech. in Computer Engineering",
+      logo_path: "ldce.png",
+      alt_name: "LDCE",
+      duration: "2016 - 2019",
+      descriptions: [
+        "⚡ 7.3 CGPA",
+        "⚡ During my BTech in Computer Engineering, I excelled academically, maintaining a high GPA and receiving recognition for my outstanding performance. I completed a range of relevant coursework, including computer architecture, data structures, algorithms, programming languages, operating systems, computer networks, and digital logic design..",
+      ],
+      website_link: "https://ldce.ac.in/",
+    },
+    {
+      title: "C U Shah University, Surendranagar",
+      subtitle: "Diploma in Computer Engineering",
+      logo_path: "cushah.jpg",
+      alt_name: "DAL",
+      duration: "2013 - 2016",
+      descriptions: [
+        "⚡ 8.5 CGPA",
+        "⚡ During my Computer Engineering Diploma course, I gained a comprehensive understanding of the fundamentals of computer engineering. I consistently demonstrated strong academic performance, earning recognition for my dedication and commitment to learning.",
+        "⚡ Throughout the diploma course, I completed coursework that encompassed essential subjects such as computer programming, digital electronics, computer networks, database management, and software engineering. These courses provided me with a solid foundation in the core principles and practices of computer engineering.",
+      ],
+      website_link: "https://www.cushahuniversity.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "React Js [2022]",
+      subtitle: "Udemy",
+      logo_path: "udemy.png",
+      certificate_link:
+        "https://www.udemy.com/certificate/UC-2df5a270-1e1c-4edf-8ff1-08e1de8cdec4/",
+      alt_name: "ReactJS",
+      color_code: "rgb(227, 64, 95)",
+    },
+    {
+      title:
+        "Modern HTML & CSS From The Beginning [ Including SASS & SCSS ] [2022]",
+      subtitle: "Udemy",
+      logo_path: "udemy.png",
+      certificate_link:
+        "https://www.udemy.com/certificate/UC-ed56282a-a83e-4ff4-a281-e4bd99b47acc/",
+      alt_name: "HTML&CSS",
+      color_code: "#1e70c1",
+    },
+    {
+      title: "Modern JavaScript [2022]",
+      subtitle: "Udemy",
+      logo_path: "udemy.png",
+      certificate_link:
+        "https://www.udemy.com/certificate/UC-0ab4ba1b-0ff5-4dc2-ac4b-baca7371e6c2/",
+      alt_name: "JAvascript",
+      color_code: "rgb(227, 64, 95)",
+    },
+    {
+      title: "BootStrap [2022]",
+      subtitle: "Udemy",
+      logo_path: "udemy.png",
+      certificate_link:
+        "https://www.udemy.com/certificate/UC-6374d852-e3f9-4aff-961d-30a151576068/",
+      alt_name: "BootStrap",
+      color_code: "#1e70c1",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Traniee, Internship",
+  description:
+    "I've completed one internship and wokred as a trainee also. I've mostly done 2+ projects during the training probation periodand I am actively looking for new job opportunity.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Worked As a Trainee",
+      experiences: [
+        {
+          title: "ReactJs Developer",
+          company: "Matlab Infotech",
+          company_url: " ",
+          logo_path: "coding.png",
+          duration: "October 2022 - May 2023",
+          location: "Surat, Gujarat",
+          description:
+            "Worked as a Trainee Software Developer, Maintained good working relationship with coworkers and management, Collaborated with the development team to implement smaller functionality and resolve bugs, ensuring smooth operation and enhancing the overall user experience, Completed assigned tasks with little or no supervision and Worked on Two Live Projects: 1. NistulaLife 2. Chefs-a-porter. More details regards this is mentioned in resume.",
+          color: "#f10000",
+        },
+      ],
+    },
+    {
+      title: "Internship",
+      experiences: [
+        {
+          title: "Summer Inernship",
+          company: "Way To Way Pvt Ltd, ahmedabad",
+          company_url: " ",
+          logo_path: "coding.png",
+          duration: "June 2018",
+          location: "Ahmedabad, Gujarat",
+          description:
+            "learnt HTML, CSS, JAVASCRIPT and created Blogs based on this",
+          color: "#ee3c26",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to made ReactJs and Wordpress projects. Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try my best to write a blog about it.",
+    link: " ",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      name: "Chefs A Porter",
+      url: " ",
+      description:
+        "Chef's A Porter is a GatsbyJS-based web platform that offers meal planning, private and ticketed dining services.",
+      languages: [
+        {
+          name: "ReactJs",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "GatbyJs",
+          iconifyClass: "logos-gatsby",
+        },
+        {
+          name: "Material Ui",
+          iconifyClass: "logos-Mui",
+        },
+      ],
+    },
+    {
+      name: "Nistula Life",
+      url: " ",
+      description:
+        "Nistula Life is a web-based platform that offers resort booking services to users.",
+      languages: [
+        {
+          name: "ReactJs",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos-bootstrap",
+        },
+      ],
+    },
+    {
+      name: "Recipe Blog",
+      url: " ",
+      description:
+        "Established and maintained a vibrant and comprehensive food recipe blog, kitchenRecipes, using the WordPress platform.",
+      languages: [
+        {
+          name: "Wordpress",
+          iconifyClass: "logos-wordpress",
+        },
+      ],
+    },
+    {
+      name: "Ecucational Blog",
+      url: " ",
+      description:
+        "Established and maintained a vibrant and comprehensive Educational blog, KnowledgeAdda, using the WordPress platform.",
+      languages: [
+        {
+          name: "Wordpress",
+          iconifyClass: "logos-wordpress",
+        },
+      ],
+    },
+    {
+      name: "Online Auction",
+      url: " ",
+      description:
+        "Designed and developed an online auction platform, leveraging technology to facilitate the buying and selling of goods and services through a digital marketplace.",
+      languages: [
+        {
+          name: "PHP",
+          iconifyClass: "logos-php",
+        },
+      ],
+    },
+    {
+      name: "CMS For PhotoGrapher",
+      url: " ",
+      description:
+        "Developed and implemented a user-friendly Content Management System (CMS) for photographers, enabling efficient content management, image editing, and secure client access.",
+      languages: [
+        {
+          name: "PHP",
+          iconifyClass: "logos-php",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
